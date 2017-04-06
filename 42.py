f96022c60dc1aefd2252d07ebdcbc92feb870a26
Andriy2 <andruha.bakay1997@gmail.com>
2017-04-06 21:03:51 +0300

    Cleared comments

--- 42.py
+++ 42.py
@@ -17,32 +17,20 @@ def find_angle(x1, y1, x2, y2, x3, y3, x4, y4):
     yy2 = y4-y3
     cos = (xx1*xx2+yy1*yy2) / ( (xx1**2 + yy1**2)**0.5 * (xx2**2 + yy2**2)**0.5 )
     sin = (1 - cos**2)**0.5
-    return sin if sin > 0 else -sin
-'''
-    if y2 == y1 and y4 == y3:
-        return False
-    else:
-        try:
-            k1 = (x2 - x1) / (y2 - y1)
-        except ZeroDivisionError:
-            k1 = float('inf')
-        try:
-            k2 = (x4 - x3) / (y4 - y3)
-        except ZeroDivisionError:
-            k2 = float('inf')
-        if k1 == k2:
-            return False
-        else:
-            return k1, k2
-'''
+    return sin
 
 
 def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
-    def find():
+    def find(x1, y1, x2, y2, x3, y3, x4, y4):
         x = -((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) / (
             (y1 - y2) * (x4 - x3) - (y3 - y4) * (x2 - x1))
         y = ((y3 - y4) * -x - (x3 * y4 - x4 * y3)) / (x4 - x3)
-        return x, y
+        if (x1 <= x <= x2 or x2 <= x <= x1) and (x3 <= x <= x4 or x4 <= x <= x3) and (y1 <= y <= y2 or y2 <= y <= y1) and (
+                            y3 <= y <= y4 or y4 <= y <= y3):
+            return [x, y]
+        else:
+            return False
+
 
     def findv(xx1, yy1, xx2, yy2, xx):
         y = - ((xx1 * yy2 - xx2 * yy1) + (yy1 - yy2) * xx) / (xx2 - xx1)
@@ -66,13 +54,7 @@ def find_intersection(x1, y1, x2, y2, x3, y3, x4, y4):
     elif x3 == x4:
         return findv(x1, y1, x2, y2, x3)
     else:
-        x, y = find()
-
-    if (x1 <= x <= x2 or x2 <= x <= x1) and (x3 <= x <= x4 or x4 <= x <= x3) and (y1 <= y <= y2 or y2 <= y <= y1) and (
-                        y3 <= y <= y4 or y4 <= y <= y3):
-        return [x, y]
-    else:
-        return False
+        return find(x1, y1, x2, y2, x3, y3, x4, y4)
 
 
 def point_in_triangle(point, triangle) -> bool:
@@ -86,15 +68,10 @@ def point_in_triangle(point, triangle) -> bool:
     return [False, True][result]
 
 
-has_just_point = False
 points = []
 first = make_triangle( input().split() or ' 1 -2  1 2 -1 0'.split() or '2 2  2 6  8 4'.split() or '0 2 1 1 2 4'.split())
 second = make_triangle(input().split() or '-1 -2 -1 2  1 0'.split() or '5 4 11 6 11 2'.split() or '2 1 3 3 1 4'.split())
-'''
-print(first)
-print(second)
-print()
-'''
+
 for i in range(3):
     for j in range(3):
         point = find_intersection(first[0][i], first[1][i], first[0][i - 1], first[1][i - 1], second[0][j],
@@ -105,7 +82,6 @@ triangles = [first, second]
 for tri in range(2):
     for i in range(3):
         if point_in_triangle([triangles[tri][0][i], triangles[tri][1][i]], triangles[tri - 1]):
-            has_just_point = True
             points.append([triangles[tri][0][i], triangles[tri][1][i]])
 S = 0
 points = filt(points)
@@ -117,5 +93,5 @@ elif len(points) == 4:
     elif find_intersection(*points[0], *points[3], *points[1], *points[2]):
         points[1], points[0] = points[0], points[1]
     S = 0.5 * length(*points[0], *points[2]) * length(*points[1], *points[3]) * find_angle(*points[0], *points[2], *points[1], *points[3])
-# TODO 6 points
+# TODO 5-6 points
 print(len(points), round(S if S > 0 else -S, 2))
