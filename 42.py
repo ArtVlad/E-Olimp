a94b33aa0c9fd3e27fb8da18aa7f837d7235be47
Andriy2 <andruha.bakay1997@gmail.com>
2017-04-06 03:09:56 +0300

    Filter point duplicates

--- 42.py
+++ 42.py
@@ -2,6 +2,10 @@ def make_triangle(arr) -> list:
     return list(map(lambda pair: list(map(int, pair)), [[arr[0], arr[2], arr[4]], [arr[1], arr[3], arr[5]]]))
 
 
+def filt(pointers):
+    return [x for i, x in enumerate(pointers) if pointers.index(x) == i]
+
+
 def length(x1,y1,x2,y2):
     return ((x2-x1)**2+(y2-y1)**2)**0.5
 
@@ -13,7 +17,7 @@ def find_angle(x1, y1, x2, y2, x3, y3, x4, y4):
     yy2 = y4-y3
     cos = (xx1*xx2+yy1*yy2) / ( (xx1**2 + yy1**2)**0.5 * (xx2**2 + yy2**2)**0.5 )
     sin = (1 - cos**2)**0.5
-    return sin
+    return sin if sin > 0 else -sin
 '''
     if y2 == y1 and y4 == y3:
         return False
@@ -84,8 +88,8 @@ def point_in_triangle(point, triangle) -> bool:
 
 has_just_point = False
 points = []
-first = make_triangle( input().split() or '2 2  2 6  8 4'.split() or '0 2 1 1 2 4'.split())
-second = make_triangle(input().split() or '5 4 11 6 11 2'.split() or '2 1 3 3 1 4'.split())
+first = make_triangle( input().split() or ' 1 -2  1 2 -1 0'.split() or '2 2  2 6  8 4'.split() or '0 2 1 1 2 4'.split())
+second = make_triangle(input().split() or '-1 -2 -1 2  1 0'.split() or '5 4 11 6 11 2'.split() or '2 1 3 3 1 4'.split())
 '''
 print(first)
 print(second)
@@ -104,6 +108,7 @@ for tri in range(2):
             has_just_point = True
             points.append([triangles[tri][0][i], triangles[tri][1][i]])
 S = 0
+points = filt(points)
 if len(points) == 3:
     S = 0.5 * ((points[0][0] - points[2][0]) * (points[1][1] - points[2][1]) - (points[1][0] - points[2][0]) * (points[0][1] - points[2][1]))
 elif len(points) == 4:
@@ -112,5 +117,5 @@ elif len(points) == 4:
     elif find_intersection(*points[0], *points[3], *points[1], *points[2]):
         points[1], points[0] = points[0], points[1]
     S = 0.5 * length(*points[0], *points[2]) * length(*points[1], *points[3]) * find_angle(*points[0], *points[2], *points[1], *points[3])
-
-print(len(points), round(S if S > 0 else -S, 2))
+# TODO 6 points
+print(len(points), round(S, 2))
